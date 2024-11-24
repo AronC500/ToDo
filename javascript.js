751f41862e20171cdadfb381f3ad4137dc027132
AronC500 <aronchen500@gmail.com>
2024-11-23 22:27:10 -0500

    G

--- javascript.js
+++ javascript.js
@@ -1,5 +1,8 @@
 const newTask = document.querySelector("#add");
 const sort = document.querySelector("#sort");
 
-newTask.addEventListener("click", () => {});
+newTask.addEventListener("click", () => {
+  const popup = document.querySelector("#popup");
+  popup.style.display = "block";
+});
 sort.addEventListener("click", () => {});
